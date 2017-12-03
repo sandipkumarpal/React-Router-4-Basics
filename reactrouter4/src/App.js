f13d454d2513dc9c0d779fd2ed99c5ab9357054e
Sandip Pal <sandip.pal18@gmail.com>
2017-12-03 13:26:17 +0530

    Added React Router Html Design & added React router package & added ESLint configaration

--- reactrouter4/src/App.js
+++ reactrouter4/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import Header from './components/Header';
+
+import Home from './components/Home';
+import About from './components/About';
+import Teachers from './components/Teachers';
+import Courses from './components/Courses';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+            <div className="container">
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/teachers" component={Teachers} />
+                    <Route path="/courses" component={Courses} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
   }
 }
---
+++ reactrouter4/src/components/Teachers.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TeacherList from '../data/teachers';
+
+const Teachers = () => {
+  const teachers = TeacherList.map(teacher => (
+      <li className="teacher" key={teacher.id} >
+          <img className="teacher-img" src={teacher.img_src} alt="teacher" />
+          <h3>{teacher.name}</h3>
+          <p>{teacher.bio}</p>
+      </li>
+  ));
+
+  return (
+      <div className="main-content">
+          <h2>Teachers</h2>
+          <ul className="group">
+              {teachers}
+          </ul>
+      </div>
+  );
+};
+
+export default Teachers;
